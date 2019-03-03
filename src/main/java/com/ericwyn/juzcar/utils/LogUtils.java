f0a1bc9a4d57a6edd81d1063c9a9aac47711b5a3
Ericwyn <Ericwyn.chen@gmail.com>
2019-03-03 16:31:28 +0800

    feat: add log utils

---
+++ src/main/java/com/ericwyn/juzcar/utils/LogUtils.java
@@ -0,0 +1,37 @@
+package com.ericwyn.juzcar.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by Ericwyn on 19-3-3.
+ */
+public class LogUtils {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+
+    private static enum Level{
+        DEBUG("DEBUG"),
+        ERROR("ERROR");
+
+        private String level;
+        Level(String level){
+            this.level = level;
+        }
+
+        @Override
+        public String toString() {
+            return this.level;
+        }
+    }
+
+    private static String logTitle(){
+        return "[ Juzcar LOG] [ DEBUG ]"+sdf.format(new Date());
+    }
+    private static String logTitle(Level level){
+        return "[ Juzcar LOG] [ "+level+" ]"+sdf.format(new Date());
+    }
+
+    public static void SOUT(String logMsg){
+        System.out.println(logTitle(Level.DEBUG)+" "+logMsg);
+    }
+}
