ea80e8451d443944ecb6138849e060488a7443b6
Ericwyn <Ericwyn.chen@gmail.com>
2019-03-03 16:35:15 +0800

    feat: add default Scanner CallBack

--- src/main/java/com/ericwyn/juzcar/JuzcarServer.java
+++ src/main/java/com/ericwyn/juzcar/JuzcarServer.java
@@ -1,6 +1,5 @@
 package com.ericwyn.juzcar;
 
-import com.alibaba.fastjson.JSONObject;
 import com.ericwyn.ezerver.expection.WebServerException;
 import com.ericwyn.juzcar.scan.ScannerUtils;
 import com.ericwyn.juzcar.scan.cb.JuzcarScannerCb;
@@ -8,6 +7,7 @@ import com.ericwyn.juzcar.scan.obj.JuzcarApi;
 import com.ericwyn.juzcar.scan.obj.JuzcarClass;
 import com.ericwyn.juzcar.scan.obj.JuzcarMethod;
 import com.ericwyn.juzcar.server.JuzcarDocServer;
+import com.ericwyn.juzcar.utils.JuzcarLogs;
 
 import java.util.HashMap;
 import java.util.List;
@@ -58,14 +58,45 @@ public class JuzcarServer {
             // TODO 如果交流的方式并非 JSON 而是 XML ？（暂时不考虑支持 xml 了
             // TODO 增加一些注解能够强硬设置API 的信息，注解在方法，替换掉自动分析注解得到的 api 信息
             // TODO 增加一个注解来设置 Controller 的名字，注解在类，避免 Controller 名字太长
+
+            // TODO server 默认不开启日志,使用 debug Flag 开启日志
         }
     };
 
+
+    /**
+     * 启动函数，cb 用来查看那些扫描得到的 api
+     *
+     * @param initClass
+     * @param juzcarScanner
+     */
     public static void run(Class initClass, JuzcarScannerCb juzcarScanner){
         JuzcarServer.initClass = initClass;
         JuzcarServer.scannerCb = juzcarScanner;
         new Thread(juzcarRunnable).run();
     }
+
+    /**
+     * 默认启动函数， 缺省的 cb
+     * @param initClass
+     */
+    public static void run(Class initClass){
+        JuzcarServer.initClass = initClass;
+        JuzcarServer.scannerCb = defaultScannerCb;
+        new Thread(juzcarRunnable).run();
+    }
+
+    /**
+     * 缺省 CB
+     */
+    private static JuzcarScannerCb defaultScannerCb = new JuzcarScannerCb() {
+        @Override
+        public void callback(HashMap<String, List<JuzcarApi>> apis) {
+            for (String key : apis.keySet()){
+                JuzcarLogs.SOUT("扫描 "+ key+" 得到 "+apis.get(key).size()+" 接口");
+            }
+        }
+    };
 
     public static HashMap<String, List<JuzcarApi>> getApis() {
         return apis;
