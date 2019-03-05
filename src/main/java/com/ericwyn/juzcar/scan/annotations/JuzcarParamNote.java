9935a578f67724e9f7eaadc209fec8bb801f8764
Ericwyn <Ericwyn.chen@gmail.com>
2019-03-05 17:01:02 +0800

    feat: @JuzcarParamNote can use in field

--- src/main/java/com/ericwyn/juzcar/scan/annotations/JuzcarParamNote.java
+++ src/main/java/com/ericwyn/juzcar/scan/annotations/JuzcarParamNote.java
@@ -9,7 +9,7 @@ import java.lang.annotation.Target;
  * 该注解用来备注 Method
  * Created by Ericwyn on 19-3-3.
  */
-@Target({ElementType.PARAMETER})
+@Target({ElementType.PARAMETER, ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JuzcarParamNote {
     String value() default "";
