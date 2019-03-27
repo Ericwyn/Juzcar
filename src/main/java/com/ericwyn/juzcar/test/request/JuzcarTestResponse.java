45f164c0eeffac4511433b3731ae477faa214ca3
Ericwyn <Ericwyn.chen@gmail.com>
2019-03-27 17:02:25 +0800

    feat: update JuzcarTestResponse

--- src/main/java/com/ericwyn/juzcar/test/request/JuzcarTestResponse.java
+++ src/main/java/com/ericwyn/juzcar/test/request/JuzcarTestResponse.java
@@ -6,4 +6,44 @@ package com.ericwyn.juzcar.test.request;
  * Created by Ericwyn on 19-3-23.
  */
 public class JuzcarTestResponse {
+    private JuzcarTestRequest request;
+    private String method;
+    private String respText;
+    private Integer statusCode;
+
+    public JuzcarTestResponse(JuzcarTestRequest request) {
+        this.request = request;
+    }
+
+    public JuzcarTestRequest getRequest() {
+        return request;
+    }
+
+    public void setRequest(JuzcarTestRequest request) {
+        this.request = request;
+    }
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public String getRespText() {
+        return respText;
+    }
+
+    public void setRespText(String respText) {
+        this.respText = respText;
+    }
+
+    public Integer getStatusCode() {
+        return statusCode;
+    }
+
+    public void setStatusCode(Integer statusCode) {
+        this.statusCode = statusCode;
+    }
 }
